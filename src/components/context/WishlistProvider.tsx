c82595ee42457d1bc738b99691fd679e9c81dd26
Ridwan <ridhwan.alabi@gmail.com>
2024-02-18 20:37:38 +0100

    Add Wishlist Provider

---
+++ src/components/context/WishlistProvider.tsx
@@ -0,0 +1,82 @@
+import { ReactElement, useMemo, useReducer } from "react";
+import { ProductType } from "./ProductsProvider";
+import { createContext } from "vm";
+
+type WishlistStateType = { wishlist: ProductType[] }
+
+const initWishlistState: WishlistStateType = { wishlist: [] }
+
+const REDUCER_ACTION_TYPE = {
+  ADD: "ADD",
+  REMOVE: "REMOVE"
+}
+
+export type ReducerActionType = typeof REDUCER_ACTION_TYPE
+
+export type ReducerAction = {
+  type: string,
+  payload?: ProductType,
+}
+
+const reducer = (state: WishlistStateType, action: ReducerAction): WishlistStateType => {
+  switch (action.type) {
+    case REDUCER_ACTION_TYPE.ADD: {
+      if (!action.payload) {
+        throw new Error('action.payload missing in ADD action')
+      }
+
+      const { name, price, id, category, description, rating, reviews } = action.payload
+
+      const filteredWishlist: ProductType[] = state.wishlist.filter(item => item.id != id)
+      
+      return { ...state, wishlist: [...filteredWishlist, { name, price, id, category, description, rating, reviews }] }
+
+    }
+    case REDUCER_ACTION_TYPE.REMOVE: {
+      if (!action.payload) {
+        throw new Error('action.payload missing in REMOVE action')
+      }
+
+      const { id } = action.payload
+
+      const filteredWishlist: ProductType[] = state.wishlist.filter(item => item.id != id)
+
+      return { ...state, wishlist: [...filteredWishlist] }
+
+    }
+    default:
+      throw new Error('Undefined reducer action type')
+  }
+}
+
+const useWishlistContext = (initWishlistState: WishlistStateType) => {
+  const [state, dispatch] = useReducer(reducer, initWishlistState)
+
+  const REDUCER_ACTIONS = useMemo(() => {
+    return REDUCER_ACTION_TYPE
+  }, [])
+
+  const wishlist = state.wishlist
+
+  return {dispatch, REDUCER_ACTIONS, wishlist}
+}
+
+export type UseWishlistContextType = ReturnType<typeof useWishlistContext>
+
+const initWishlistContextState: UseWishlistContextType = {
+  dispatch: () => { },
+  REDUCER_ACTIONS: REDUCER_ACTION_TYPE,
+  wishlist: []
+}
+
+export const WishlistContext = createContext(initWishlistContextState)
+
+type ChildrenType = { children?: ReactElement | ReactElement[] }
+
+export const WishlistProvider = ({ children }: ChildrenType): ReactElement => {
+  return (
+    <WishlistContext.Provider value={useWishlistContext(initWishlistState)}>
+      {children}
+    </WishlistContext.Provider>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.min.css';
 import './index.css'
 import { ProductsProvider } from './components/context/ProductsProvider.tsx';
+import { WishlistProvider } from './components/context/WishlistProvider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ProductsProvider>
-      <App />
+      <WishlistProvider>
+        <App />
+      </WishlistProvider>
     </ProductsProvider>
   </React.StrictMode>,
 )
