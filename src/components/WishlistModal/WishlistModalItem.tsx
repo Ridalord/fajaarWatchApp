b10fafa5ff9edbaabb5ce669bad630ab62a91844
Ridwan <ridhwan.alabi@gmail.com>
2024-02-25 13:59:39 +0100

    Pictures dynamically generated on wishlist modal

--- src/components/WishlistModal/WishlistModalItem.tsx
+++ src/components/WishlistModal/WishlistModalItem.tsx
@@ -2,10 +2,15 @@ import { CartItemType } from "../context/CartProvider"
 import useCart from "../hooks/useCart"
 import useWishlist from "../hooks/useWishlist"
 import { X } from "react-bootstrap-icons";
-import Ticwatch from "./ticwatch.jpeg"
+import { getStorage, ref, getDownloadURL, list } from "firebase/storage";
+import { useEffect, useState } from "react";
 
 type PropTypes = {
   product: CartItemType,
+}
+
+interface ImageUrls {
+  [productId: string]: string[];
 }
 
 export default function WishlistModalItem({ product }: PropTypes) {
@@ -17,12 +22,42 @@ export default function WishlistModalItem({ product }: PropTypes) {
   }
   const removeFromWishlist = () => wishlistDispatch({ type: WISHLIST_REDUCER_ACTION.REMOVE, payload: {...product} })
 
+  const fetchProductImageUrls = (productId: string) => {
+    const storage = getStorage();
+    const productRef = ref(storage, `Products/${productId}`);
+    return list(productRef)
+      .then((items) => {
+        const imagePromises = items.items.map((item) => {
+          return getDownloadURL(item);
+        });
+        return Promise.all(imagePromises);
+      })
+      .catch((error) => {
+        console.error("Error fetching product image URLs:", error);
+        return [];
+      });
+  };
+
+  const [imageUrls, setImageUrls] = useState<ImageUrls>({});
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      const urls: ImageUrls = {};
+
+      const urlsForProduct = await fetchProductImageUrls(product.id);
+      urls[product.id] = urlsForProduct;
+
+      setImageUrls(urls);
+    };
+    fetchImages();
+  }, [product]);
+
   return (
     <tr>
       <td>
         <div className="cart-product">
           <div className="cart-product__img">
-            <img src={Ticwatch} alt={product.name} />
+            <img src={imageUrls[product.id]?.[0] || "#"} alt={product.name} />
           </div>
           <div className="cart-product__txt">
             <h6><a href="shop-details.html">{product.name}</a></h6>
