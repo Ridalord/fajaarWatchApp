eec84a7cc70033528183bff093a25337090fc6cc
Ridwan <ridhwan.alabi@gmail.com>
2024-05-07 14:49:01 +0100

    Add contact page and linked to firestore

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,9 +1,53 @@
 import Breadcrumbs from "../Breadcrumbs/Breadcrumbs";
+import ContactForm from "./ContactForm";
 
 export default function Contact() {
   return (
     <>
-      <Breadcrumbs page="contact"/>
+      <Breadcrumbs page="contact" />
+      <div className="container">
+        <div className="fz-inner-contact-details">
+
+          <ContactForm/>
+
+          <div className="fz-inner-contact-details__info-card">
+            <h3 className="fz-inner-contact-details__title">Get In Touch</h3>
+            <ul>
+              <li className="fz-single-contact-info">
+                <i className="fa-light fa-location-dot"></i>
+                <span className="contact-address">989 Bel Meadow Drive Los Angeles, CA 90017</span>
+              </li>
+
+              <li className="fz-single-contact-info">
+                <i className="fa-light fa-phone"></i>
+                <div className="contact-numbers">
+                  <span><a href="tel:+2348132246772">(+234) 813-224-6772</a></span>
+                  {/* <span><a href="tel:470-142-2412">(+1) 470-142-2412</a></span> */}
+                </div>
+              </li>
+
+              <li className="fz-single-contact-info">
+                <i className="fa-light fa-clock"></i>
+                <div className="contact-numbers">
+                  <span className="store-time__title">Store Hours:</span>
+                  <span className="store-time__hours">Mon - Sat : 8:00am - 5:00pm</span>
+                </div>
+              </li>
+
+              <li className="contact-socials">
+                <h6 className="contact-socials__title">Follow Us:</h6>
+                <ul>
+                  <li><a href="#"><i className="fa-brands fa-facebook-f"></i></a></li>
+                  <li><a href="#"><i className="fa-brands fa-twitter"></i></a></li>
+                  <li><a href="#"><i className="fa-brands fa-instagram"></i></a></li>
+                  <li><a href="#"><i className="fa-brands fa-youtube"></i></a></li>
+                  <li><a href="#"><i className="fa-brands fa-tiktok"></i></a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/components/Contact/ContactForm.tsx
@@ -0,0 +1,78 @@
+import React, { FormEvent, useState } from 'react';
+import { db } from '../../config/firebase';
+import { addDoc, collection } from 'firebase/firestore';
+
+interface FormValues {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  message: string;
+}
+
+const ContactForm: React.FC = () => {
+  const [formData, setFormData] = useState<FormValues>({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+    message: ''
+  });
+  const[loading, setLoading]= useState<boolean>(false)
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setLoading(prev=>!prev)
+    try {
+      await addDoc(collection(db, 'Messages'), formData);
+      setFormData({
+        firstName: '',
+        lastName: '',
+        email: '',
+        phoneNumber: '',
+        message: ''
+      });
+      setLoading(prev => !prev)
+    } catch (error) {
+      console.error('Error submitting form data:', error);
+    }
+  };
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = event.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  return (
+    <div className="fz-inner-contact-details__left">
+      <div className="fz-blog-details__comment-form">
+        <h4 className="fz-comment-form__title fz-inner-contact-details__title">Leave a Message</h4>
+        <form onSubmit={handleSubmit}>
+          <div className="row g-xl-4 g-3">
+            <div className="col-6 col-xxs-12">
+              <input type="text" name="firstName" value={formData.firstName} required id="commenter-first-name" placeholder="First Name" onChange={handleChange} />
+            </div>
+            <div className="col-6 col-xxs-12">
+              <input type="text" name="lastName" value={formData.lastName} required id="commenter-last-name" placeholder="Last Name" onChange={handleChange} />
+            </div>
+            <div className="col-6 col-xxs-12">
+              <input type="email" name="email" value={formData.email} required id="commenter-email" placeholder="Email Address" onChange={handleChange} />
+            </div>
+            <div className="col-6 col-xxs-12">
+              <input type="tel" name="phoneNumber" value={formData.phoneNumber} required id="commenter-number" placeholder="Phone Number" onChange={handleChange} />
+            </div>
+            <div className="col-12">
+              <textarea name="message" value={formData.message} required id="commenter-comment" placeholder="Your Message" onChange={handleChange}></textarea>
+            </div>
+          </div>
+
+          <button disabled={loading} type="submit" className="fz-1-banner-btn fz-comment-form__btn">Send Message</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ContactForm;
